20278642f1f9c3f386452fe37091adf0827337cf
Peter Saxton <peterhsaxton@gmail.com>
2014-01-07 14:42:47 +0000

    Added comment lines to makers.rb

--- makers.rb
+++ makers.rb
@@ -1,4 +1,7 @@
+# Variable assigned for the first time
 people = "Makers"
 puts "The people variable is currently: " + people
+
+# Variable overwritten
 people = "Coders"
 puts "The people variable is currently: " + people
