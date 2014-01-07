07e407cd71c84a86bed77caf01af5b91f97657d8
Peter Saxton <peterhsaxton@gmail.com>
2014-01-07 14:49:54 +0000

    string conversion is automatic in interpolation

--- makers.rb
+++ makers.rb
@@ -8,3 +8,8 @@ puts "The people variable is currently: #{people}"
 
 # Interpolation for numerical expression
 puts "2^3 = #{2**3}"
+
+#Interpolation does string conversion
+hour = 10
+puts hour.class
+puts "The time is #{hour}"
